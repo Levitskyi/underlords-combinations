ef12b5d36f4e5b663c8a75a2349d7c058d6b5fa6
Serhii Skobel <S.Skobel05@gmail.com>
2019-07-15 14:06:10 +0300

    Added: classes images, animation on hover. Changed: background image.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Heroes from './Components/Heroes';
-import './Styles/Heroes.css';
+import './Styles/heroes.css';
 
 class App extends Component {
 
--- src/Components/Heroes.js
+++ src/Components/Heroes.js
@@ -2,31 +2,35 @@ import React, { Component } from 'react';
 
 class Heroes extends Component {
 	state = {
-  	heroes: []
+		heroes: []
 	};
 
-  componentDidMount() {
-  	fetch('/heroes')
-	    .then(res => res.json())
-	    .then(({ data }) => {
-	      console.log(data);
-	      this.setState( { heroes: data });
-	    })
+	componentDidMount() {
+		fetch('/heroes')
+			.then(res => res.json())
+			.then(({ data }) => {
+				console.log(data);
+				this.setState( { heroes: data });
+			})
 	}
 
 	render() {
-		return (
-			<div className="App">
-					{this.state.heroes.map(hero =>
-		        <div key={hero._id} className="heroes-images">
-							<img alt={hero.name} src={hero.imgUrl}/>
-							<p className="heroes-name white-color">{hero.name.toUpperCase()}</p>
-							<p className="heroes-class white-color">{hero.classList.map(i => i + ' ')}</p>
-		        </div>
-	      	)}
-			</div>
-		)
-	}
+  	return (
+  		<div className="heroes-list">
+  			{this.state.heroes.map(hero =>
+  				<div key={hero._id} className="heroes-list__block">
+  					<div className="heroes-list__heroes-images">
+  						<img alt={hero.name} src={hero.imgUrl}/>
+						</div>
+  					<h2 className="heroes-list__heroes-name white-color">{hero.name.toUpperCase()}</h2>
+  					<div className="heroes-list__heroes-class">
+  						{hero.classList.map(hero_class => <img alt={hero_class._id} src={hero_class.imgUrl}/>)}
+						</div>
+  				</div>
+  			)}
+  		</div>
+  	)
+  }
 }
 
 export default Heroes;

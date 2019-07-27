2e031271a9a26ee2f772bd2922eacbf84501c345
Serhii Skobel <S.Skobel05@gmail.com>
2019-07-27 20:35:32 +0300

    Minor changes.

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -31,45 +31,44 @@ class Hero extends Component {
             return <div className="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         }
     
-    return (
-        <div className="hero-block">
-            <div className="hero-block__hero-image">
-                <img key={hero._id} alt={hero.name} src={`/${hero.imgUrl}`} />
-            </div>
-            <div className="hero-block__hero-info">
-                <h1 className="hero-block__hero-name white-color text-uppercase">{hero.name}</h1>
-                <h2 className="hero-block__hero-group white-color text-uppercase">{hero.group}</h2>
-                <div className="hero-block__hero-classes">
-                    {hero.classList.map(hero_class => <img key={hero_class._id} alt={hero_class.name} src={`/${hero_class.imgUrl}`} />)}
+        return (
+            <div className="hero-block">
+                <div className="hero-block__hero-image">
+                    <img key={hero._id} alt={hero.name} src={`/${hero.imgUrl}`} />
                 </div>
-                <div className="hero-block__hero-detail-info">
-                    <div className="hero-block__hero-toggle-buttons white-color">
-                        <button onClick={() => this.toggleButton("ability")} className={activeTab === "ability" ? "active-button" : null}>ABILITY</button>
-                        <button onClick={() => this.toggleButton("stats")} className={activeTab === "stats" ? "active-button" : null}>STATS</button>
-                        <button onClick={() => this.toggleButton("test")} className={activeTab === "test" ? "active-button" : null}>TEST</button>
+                <div className="hero-block__hero-info">
+                    <h1 className="hero-block__hero-name white-color text-uppercase">{hero.name}</h1>
+                    <h2 className="hero-block__hero-group white-color text-uppercase">{hero.group}</h2>
+                    <div className="hero-block__hero-classes">
+                        {hero.classList.map(hero_class => <img key={hero_class._id} alt={hero_class.name} src={`/${hero_class.imgUrl}`} />)}
                     </div>
-                    { activeTab === "ability" ?
-                        (<div className={"hero-block__hero-ability-info"}>
-                            <img alt={hero.ability.name} src={`/${hero.ability.imgUrl}`} />
-                            <div className="hero-block__ability-description white-color">
-                                <h3 className="text-uppercase">{hero.ability.name}</h3>
-                                <p>{hero.ability.description}</p>
-                            </div>
-                        </div>)
-                    : activeTab === "stats" ?
-                        (<div className={"hero-block__hero-stats-info"} >
-                            <h4 className="white-color text-uppercase">Level</h4>
-                            <div className="hero-block__stats-description">
-                                {Object.keys(hero.stats).map(stat => <p>{stat} {hero.stats[stat].join(" ")}</p>)}
-                            </div>
-                        </div>)
-                    : null
-                    }
+                    <div className="hero-block__hero-detail-info">
+                        <div className="hero-block__hero-toggle-buttons white-color">
+                            <button onClick={() => this.toggleButton("ability")} className={activeTab === "ability" ? "active-button" : null}>ABILITY</button>
+                            <button onClick={() => this.toggleButton("stats")} className={activeTab === "stats" ? "active-button" : null}>STATS</button>
+                        </div>
+                        { activeTab === "ability" &&
+                            (<div className={"hero-block__hero-ability-info"}>
+                                <img alt={hero.ability.name} src={`/${hero.ability.imgUrl}`} />
+                                <div className="hero-block__ability-description white-color">
+                                    <h3 className="text-uppercase">{hero.ability.name}</h3>
+                                    <p>{hero.ability.description}</p>
+                                </div>
+                            </div>)
+                        }
+                        { activeTab === "stats" &&
+                            (<div className={"hero-block__hero-stats-info"} >
+                                <h4 className="white-color text-uppercase">Level</h4>
+                                <div className="hero-block__stats-description">
+                                    {Object.keys(hero.stats).map(stat => <p>{stat} {hero.stats[stat].join(" ")}</p>)}
+                                </div>
+                            </div>)
+                        }
+                    </div>
                 </div>
-            </div>
-      </div>
-    );
-  }
+          </div>
+        );
+      }
 }
 
 export default Hero;
